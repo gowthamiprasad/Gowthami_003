89e727c85c6a683a4870bd9b85a51129cc6a913f
psivareddy <psivareddy@netflix.com>
2015-04-01 16:49:05 -0700

    removed unwanted file

--- src/com/comcast/interview/taxperson/CalculateTax.java
+++ src/com/comcast/interview/taxperson/CalculateTax.java
@@ -1,8 +1,29 @@
 package com.comcast.interview.taxperson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Gowthami on 4/1/15.
  */
 public class CalculateTax {
+
+
+    //Assumption is price is always in pennies and question says single item. So not using list here
+    public  Long calculate(int price, boolean isLuxury) throws ZeroLessThanZeroException{
+        if(price <= 0){
+            throw new ZeroLessThanZeroException("Price Cannot be '0' or less than '0'. The value user entered is : " + price);
+        }
+        long finalPrice;
+            if(!isLuxury){
+                finalPrice = (long) Math.abs(price*1.01);
+            } else{
+                finalPrice = (long) Math.abs(price*1.09);
+            }
+
+        return finalPrice;
+    }
+
+
 
 }
